e67c660b20f1219add599cdf31758ee0e3790175
Kelsey Fecho <kfecho30@gmail.com>
2018-06-26 13:07:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ function showRepositories(event, data) {
 function getRepositories() {
   const req = new XMLHttpRequest()
   const user = document.getElementById('username-field').value;
-  console.log(user);
   req.addEventListener("load", showRepositories);
   req.open("GET", `https://api.github.com/users/${user}/repos`)
   req.send()
