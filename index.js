9d0b0eb7e506e9c972afeecd883d36da4fb490e1
Kelsey Fecho <kfecho30@gmail.com>
2018-06-26 13:07:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,12 +15,13 @@ function getCommits(el) {
 function showRepositories(event, data) {
   const repos = JSON.parse(this.responseText)
   const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
-document.getElementById("repositories").innerHTML = repoList
+  document.getElementById("repositories").innerHTML = repoList
 }
 
 function getRepositories() {
   const req = new XMLHttpRequest()
   const user = document.getElementById('username-field').value;
+  console.log(user);
   req.addEventListener("load", showRepositories);
   req.open("GET", `https://api.github.com/users/${user}/repos`)
   req.send()
